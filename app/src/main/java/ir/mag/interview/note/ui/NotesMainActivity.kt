433931d6909ab4d0d405fdc539ecad8c4a58ab8a
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-05-31 04:23:03 +0430

    Add data binding

---
+++ app/src/main/java/ir/mag/interview/note/ui/NotesMainActivity.kt
@@ -0,0 +1,35 @@
+package ir.mag.interview.note.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import ir.mag.interview.note.NoteApplication
+import ir.mag.interview.note.R
+import ir.mag.interview.note.databinding.ActivityNotesMainBinding
+import ir.mag.interview.note.di.notes.NotesComponent
+import ir.mag.interview.note.ui.main.NotesFragment
+import ir.mag.interview.note.util.UiUtil
+import javax.inject.Inject
+
+class NotesMainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNotesMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: ")
+        inject()
+
+        notesFragment = fragmentFactory.instantiate(classLoader, NotesFragment::class.java.name)
+
+        binding = ActivityNotesMainBinding.inflate(layoutInflater)
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupUI()
+    }
+    companion object {
+        const val FRAGMENT_TAG: String = "NOTES_MAIN_ACTIVITY_FRAGMENT"
+        private const val TAG = "Activity.NotesMainActivity"
+    }
+}
